f9f5511a31c9717ddcb5b2d447a40d78e3743478
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-03-18 03:57:12 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+   content: [
+    './pages/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',
   ],
-  darkmode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
