3167911038178539678192b916c2510addc1bfb0
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-03-20 01:15:35 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-// import Contact from "./components/Contact";
+import { Outlet, Link } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Main from "./components/Main";
+
+// import About from "./components/About";
 // import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
 // import Skills from "./components/Skills";
 // import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
-    <main>
+    <div>
+      <h1>Main App</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/main">Main</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/testimonials">Testimonials</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      {/* 
       <Navbar />
-       <About />
+      <Main />
+      <About />
+      <Projects /> */}
       {/* <Projects />
       <Skills />
       <Testimonials />
       <Contact />  */}
-    </main>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+import About from "../routes/About";
+import Contact from "../routes/Contact";
+import Main from "../routes/Main";
+import Projects from "../routes/Projects";
+import Skills from "../routes/Skills";
+import Testimonials from "../routes/Testimonials";
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="main" element={<Main />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="skills" element={<Skills />} />
+      <Route path="testimonials" element={<Testimonials />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
