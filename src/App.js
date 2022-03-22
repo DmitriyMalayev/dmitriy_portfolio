2f6c5730a81b5a17fac63b101297c187fa1e5c68
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-03-22 03:25:08 -0400

    fixed navigation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import { Outlet, Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-blue-500">Main App</h1>
-
-      <div className="bg-blue-100">
-        <Link to="/main">Main</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/testimonials">Testimonials</Link>
-        <Link to="/contact">Contact</Link>
+      <div className="space-x-2 bg-blue-200">
+        <Link className="hover:bg-sky-300" to="/main">
+          Main
+        </Link>
+        <Link className="hover:bg-sky-300" to="/about">
+          About
+        </Link>
+        <Link className="hover:bg-sky-300" to="/projects">
+          Projects
+        </Link>
+        <Link className="hover:bg-sky-300" to="/skills">
+          Skills
+        </Link>
+        <Link className="hover:bg-sky-300" to="/testimonials">
+          Testimonials
+        </Link>
+        <Link className="hover:bg-sky-300" to="/contact">
+          Contact
+        </Link>
       </div>
       <Outlet />
+      <h1 className="text-3xl font-bold underline text-blue-500">Main App</h1>
     </div>
   );
 }
