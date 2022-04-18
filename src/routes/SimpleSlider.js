a067ebed1bc011a57e2f01642256757983616645
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-04-18 19:33:21 -0400

    deploy

--- src/routes/SimpleSlider.js
+++ src/routes/SimpleSlider.js
@@ -13,15 +13,13 @@ import Image2 from "../combined_certificates/02.jpg";
 import Image3 from "../combined_certificates/03.jpg";
 import Image4 from "../combined_certificates/04.jpg";
 import Image5 from "../combined_certificates/05.jpg";
-// import Image6 from "../combined_certificates/06.jpg";
-// import Image7 from "../combined_certificates/07.jpg";
-// import Image8 from "../combined_certificates/08.jpg";
-// import Image9 from "../combined_certificates/09.jpg";
-// import Image10 from "../combined_certificates/10.jpg";
-// import Image11 from "../combined_certificates/11.jpg";
-// import Image12 from "../combined_certificates/12.jpg";
-// import Image13 from "../combined_certificates/013.jpg";
-// import Image14 from "../combined_certificates/014.jpg";
+import Image6 from "../combined_certificates/06.jpg";
+import Image7 from "../combined_certificates/07.jpg";
+import Image8 from "../combined_certificates/08.jpg";
+import Image9 from "../combined_certificates/09.jpg";
+import Image10 from "../combined_certificates/10.jpg";
+import Image11 from "../combined_certificates/11.jpg";
+import Image12 from "../combined_certificates/12.jpg";
 
 class SimpleSlider extends Component {
   render() {
@@ -32,35 +30,36 @@ class SimpleSlider extends Component {
           {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-          <img src={Image2} alt="Image1" />
+          <img src={Image2} alt="Image2" />
           {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-          <img src={Image3} alt="Image1" />
+          <img src={Image3} alt="Image3" />
           {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-          <img src={Image4} alt="Image1" />
+          <img src={Image4} alt="Image4" />
           {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-          <img src={Image5} alt="Image1" />
+          <img src={Image5} alt="Image5" />
           {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-          <img src={Image1} alt="Image1" />
+          <img src={Image6} alt="Image6" />
           {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-          <img src={Image1} alt="Image1" />
+          <img src={Image7} alt="Image7" />
           {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-          <img src={Image1} alt="Image1" />
+          <img src={Image8} alt="Image8" />
           {/* <p className="legend">Legend 1</p> */}
         </div>
+
       </Carousel>
     );
   }
 }
-export default SimpleSlider
+export default SimpleSlider;
